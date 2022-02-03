b1338be5f4512e1588cb023fe0af554aeaf06b18
Fastroof <lytvdan@gmail.com>
2022-02-03 12:51:30 +0200

    Add possibility for get dollar rate from another app

--- src/main/java/ua/com/apricotka/rate/DollarRate.java
+++ src/main/java/ua/com/apricotka/rate/DollarRate.java
@@ -1,24 +1,28 @@
 package ua.com.apricotka.rate;
 
 import org.apache.log4j.Logger;
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
+import org.json.JSONObject;
+import org.json.JSONTokener;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import ua.com.apricotka.controller.OrderInfoController;
+
+import java.net.URL;
 
 @Service
 public class DollarRate {
 
     private static final Logger log = Logger.getLogger(OrderInfoController.class);
-    @Value("${linkToNBU}")
-    private String linkToNBU;
+    @Value("${linkForUSD}")
+    private String linkForUSD;
 
     public double getRate() {
         double rate = 0;
         try {
-            Document doc = Jsoup.connect(linkToNBU).get();
-            rate = Double.parseDouble(doc.select("rate").text());
+            URL url = new URL(linkForUSD);
+            JSONTokener tokener = new JSONTokener(url.openStream());
+            JSONObject root = new JSONObject(tokener);
+            rate = Double.parseDouble(root.getString("value"));
         } catch (Exception e) {
             log.error(e);
         }
